97f35d5bbcb68483dfc9d13bfa489fc5fa95b1a7
ForeverFast <59344813+ForeverFast@users.noreply.github.com>
2021-06-01 12:08:45 +0300

    Фикс памяти (50/50)

--- obj/Debug/netcoreapp3.1/MVVM/Views/Pages/FolderPage.g.i.cs
+++ obj/Debug/netcoreapp3.1/MVVM/Views/Pages/FolderPage.g.i.cs
@@ -63,15 +63,15 @@ namespace MemeFolder.MVVM.Views.Pages {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MemeFolder;component/mvvm/views/pages/folderpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MemeFolder;V1.0.0.0;component/mvvm/views/pages/folderpage.xaml", System.UriKind.Relative);
             
-            #line 1 "..\..\..\..\..\..\MVVM\Views\Pages\FolderPage.xaml"
+            #line 1 "..\..\..\..\..\..\Mvvm\Views\Pages\FolderPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
             
             #line default
@@ -79,7 +79,7 @@ namespace MemeFolder.MVVM.Views.Pages {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
