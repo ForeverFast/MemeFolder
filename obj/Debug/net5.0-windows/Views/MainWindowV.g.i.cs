93121d0ab44b8d0d136556e1f75495b8eaa4e635
ForeverFast <59344813+ForeverFast@users.noreply.github.com>
2021-07-06 13:36:54 +0300

    temp

--- obj/Debug/net5.0-windows/Views/MainWindowV.g.i.cs
+++ obj/Debug/net5.0-windows/Views/MainWindowV.g.i.cs
@@ -145,7 +145,7 @@ namespace MemeFolder.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MemeFolder;component/views/mainwindowv.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MemeFolder;V1.0.0.0;component/views/mainwindowv.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindowV.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
